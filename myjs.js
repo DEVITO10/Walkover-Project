fd5e487969c5dcd721c27eb489b30573c34f8a56
Devito <devito@devitocorp.com>
2021-11-06 15:50:37 +0530

    created other divs

--- myjs.js
+++ myjs.js
@@ -1,4 +1,10 @@
+const ques_pool = {};
+var name = null;
+var email = null;
+
+
 window.addEventListener("load",function(){
     document.getElementById("date").textContent = new Date().toLocaleDateString();
     var settime = setInterval(() => {document.getElementById("time").textContent = new Date().toLocaleTimeString();},1000);
 });
+
